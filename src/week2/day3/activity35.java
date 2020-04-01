f6028524436269a670501352e18b5343a22f34e6
phuc1nguyen <44117816+phuc1nguyen@users.noreply.github.com>
2020-04-01 21:46:52 +0700

    modified:   src/week2/day3/activity35.java

--- src/week2/day3/activity35.java
+++ src/week2/day3/activity35.java
@@ -2,9 +2,7 @@
 
 package week2.day3;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class activity35 {
     private static Scanner sc = new Scanner(System.in);
@@ -23,7 +21,7 @@ public class activity35 {
         //1. Tinh trung binh cong cac so le o vi tri chan
         int oddSum = 0;
         int count = 0;
-        for (int i = 0; i < intArray.length; i += 2) {
+        for (int i = 0; i < intArray.length; i += 2) {  //Neu toan so chan ket qua ra NaN
             if ((intArray[i] % 2) != 0) {
                 oddSum += intArray[i];
                 count++;
@@ -41,7 +39,7 @@ public class activity35 {
         //3. Tim vi tri cac so nho nhat trong mang
         int min = intArray[0];
         ArrayList<Integer> arr = new ArrayList<>();
-        for (int i = 0; i < intArray.length; ++i) {
+        for (int i = 0; i < intArray.length; ++i) {     //Chi thoa man khi nhap so be nhat o vi tri dau tien cua mang
             if (intArray[i] < min) {
                 min = intArray[i];
             } else if (intArray[i] == min) {
@@ -54,28 +52,51 @@ public class activity35 {
         }
 
         //4. Dem cac so chinh phuong co trong mang
-        //System.out.println("4. So cac so chinh phuong co trong mang la: " + squareNum);
+        int count1 = 0;
+        for (int i = 0; i < intArray.length; ++i) {
+            double squareNum = Math.sqrt(intArray[i]);
+            if (squareNum - Math.floor(squareNum) == 0) {
+                count1++;
+            }
+        }
+        System.out.println("4. So cac so chinh phuong co trong mang la: " + count1);
 
         //5. Hien thi cac so nguyen to co trong mang
-        String primeNum = "";
-        int num = 0;
-        for (int i = 1; i <= intArray.length; ++i) {
-            int count1 = 0;
-            for(num = i; num>=1; num--) {
-                if (i % num == 0)
-                    count1 = count1 + 1;
+        System.out.print("5. Cac so nguyen to co trong mang la: ");
+        for (int i = 0; i < intArray.length; i++) {     //Chua loai truong hop nhap gia tri 0
+            boolean isPrime = true;
+            int primeNum = intArray[i];
+            for (int j = 2; j < primeNum; j++) {
+                if (primeNum % j == 0) {
+                    isPrime = false;
+                    break;
+                }
             }
-            if (count1 == 2) primeNum = primeNum + i + " ";
+            if (isPrime) {
+                System.out.print(primeNum + " ");
+            }
         }
-        System.out.println("5. Cac so nguyen to co trong mang la: " + primeNum);
+        System.out.println();
 
         //6. Thay the cac phan tu am co trong mang bang gia tri 0
-        //System.out.println("6. Mang moi sau khi thay the cac phan tu am bang gia tri 0 la: " + Arrays.toString(intArray1));
+        ArrayList<Integer> arr2 = new ArrayList<>();
+        for (int i = 0; i < intArray.length; ++i) {
+            if (intArray[i] < 0) arr2.add(0);
+            else arr2.add(intArray[i]);
+        }
+        System.out.println("6. Mang moi sau khi thay the cac phan tu am bang gia tri 0 la: " + Arrays.toString(arr2.toArray()));
 
         //7. Xoa cac phan tu am co trong mang
-        //System.out.println("7. Mang moi sau khi xoa cac phan tu am la: ");
+        ArrayList<Integer> arr4 = new ArrayList<>();
+        for (int i = 0; i < intArray.length; ++i) {
+            if (intArray[i] >= 0) arr4.add(intArray[i]);
+        }
+        System.out.println("7. Mang moi sau khi xoa cac phan tu am la: " + Arrays.toString(arr4.toArray()));
 
         //8. Sap xep mang da nhap theo thu tu giam dan
-        //System.out.println("8. Mang moi sau khi duoc sap xep giam dan la: " + Arrays.toString(intArray3));
+        ArrayList<Integer> arr3 = new ArrayList<>();
+        for (int i = 0; i < intArray.length; ++i) arr3.add(intArray[i]);
+        Collections.sort(arr3, Collections.reverseOrder());
+        System.out.println("8. Mang sau khi duoc sap xep giam dan la: " + Arrays.toString(arr3.toArray()));
     }
 }

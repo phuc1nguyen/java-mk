85813178be6fddf171498fd9199825bec6e16792
phuc1nguyen <44117816+phuc1nguyen@users.noreply.github.com>
2020-04-02 13:56:23 +0700

    Changes to be committed:

---
+++ src/HackerRank/JavaList.java
@@ -0,0 +1,37 @@
+package HackerRank;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class JavaList {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        LinkedList<Integer> jvList = new LinkedList<>();
+        for (int i = 0; i < n; ++i) {
+            jvList.add(sc.nextInt());
+        }
+
+        //Perform two queries
+        int q = sc.nextInt();       //Type '2' for 2 queries
+        for (int i = 0; i < q; ++i) {
+            sc.nextLine();
+            String action = sc.nextLine();      //Type 'insert' or 'delete'
+            if ((action.substring(0, 1).toUpperCase() + action.substring(1, action.length())).equals("Insert")) {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                jvList.add(x, y);
+            } else if ((action.substring(0, 1).toUpperCase() + action.substring(1, action.length())).equals("Delete")) {
+                int x = sc.nextInt();
+                jvList.remove(x);
+            }
+        }
+
+        //Print updated list
+        for (int item:jvList) {
+            System.out.printf("%d ", item);
+        }
+        System.out.println();
+    }
+}
--- src/HackerRank/draft.java
+++ src/HackerRank/draft.java
@@ -5,13 +5,9 @@ import java.util.Scanner;
 public class draft {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int i = sc.nextInt();
-        double d = sc.nextDouble();
-        sc.nextLine();
         String s = sc.nextLine();
 
-        System.out.println("String: " + s);
-        System.out.println("Double: " + d);
-        System.out.println("Integer: " + i);
+        System.out.println(s.substring(0, 1).toUpperCase() + s.substring(1, s.length()));
+
     }
 }
